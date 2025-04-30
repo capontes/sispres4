88f4d5733ebfb2c72b6aa118115665b5580412ee
capontes <caponte2000@hotmail.com>
2025-04-30 05:11:45 -0500

    modulos sin errorres de sintasis

--- server/src/lib/customer/domain/CustomerNroDoc.ts
+++ server/src/lib/customer/domain/CustomerNroDoc.ts
@@ -7,10 +7,6 @@ export class CustomerNroDoc {
   }
   ensureValidate() {
     if (this.value.length < 8)
-<<<<<<< HEAD
       throw Error("Nro de Documento debe tener al menos 8 Caracteres");
-=======
-      throw Error("Nro de Documento debe tener 8 Caracteres");
->>>>>>> 3da75999d642963958dd9a6d7345c614d73bdfd3
   }
 }
--- server/src/lib/customer/domain/CustomerTasaInteres.ts
+++ server/src/lib/customer/domain/CustomerTasaInteres.ts
@@ -6,11 +6,6 @@ export class CustomerTasaInteres {
   }
 
   ensureValidate() {
-<<<<<<< HEAD
-    if (this.value < 0) throw Error("Tasa de Interes debe se mayor que cero");}
-
-=======
     if (this.value < 0) throw Error("Tasa de Interes debe se mayor que cero");
   }
->>>>>>> 3da75999d642963958dd9a6d7345c614d73bdfd3
 }
